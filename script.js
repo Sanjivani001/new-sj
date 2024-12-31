2c0389fc64d56ed1e309c7a019236856507d4368
Sanjivani Jadhav <sanju010305@gmail.com.com>
2024-12-31 12:02:23 +0530

    added more pages and js file

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementsByTagName('button')[0].addEventListener('click',function(event){
+    window.location.href = 'story1.html';
+});
+document.getElementsByTagName('button')[1].addEventListener('click',function(event){
+    window.location.href = 'story2.html';
+});
+document.getElementsByTagName('button')[2].addEventListener('click',function(event){
+    window.location.href = 'story3.html';
+});
+document.getElementsByTagName('button')[3].addEventListener('click',function(event){
+    window.location.href = 'story4.html';
+});
